f457887c8d8a91c93298f986aabfaa826f9cc5db
Mac <mac@MacBook-Air-Mac.local>
2024-08-28 02:55:55 +0300

    add calendar

--- src/pages/MyCalendar.vue
+++ src/pages/MyCalendar.vue
@@ -1,13 +1,30 @@
-<script setup>
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
+export default {
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth'
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <div class="p-4 sm:ml-64 bg-bgCalendar">
-    <h2>Calendar</h2>
+    <div class="bg-white">
+      <FullCalendar :options="calendarOptions"/>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
