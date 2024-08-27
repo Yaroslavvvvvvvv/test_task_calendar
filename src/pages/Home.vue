ced12abbf7e618f16ece871e5d909a4dce89b7d3
Mac <mac@MacBook-Air-Mac.local>
2024-08-28 02:08:30 +0300

    finish header

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,9 @@
 <script setup>
 
 import LeftNavMenu from "../layouts/LeftNavMenu.vue";
-import ButtonLeftNavMenu from "../components/ui/molecules/ButtonLeftNavMenu.vue";
 </script>
 
 <template>
-  <ButtonLeftNavMenu/>
   <LeftNavMenu/>
   <router-view></router-view>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,11 @@ module.exports =  {
         source: ['Source Sans Pro', 'sans-serif'],
       },
       colors: {
-        primary: '#1DA1F2', // Custom color example
-        secondary: '#14171A', // Another custom color
-        accent: '#F5A623', // Accent color
-        customGray: '#657786', // Custom gray color
         sidebar: '#43425d',
         sidebarDark: '#35344a',
         sidebarNav: '#a3a0fb',
+        navHeader:'#4d4f5c',
+        navHeaderline:'#ebebf2',
       },
     },
   },
